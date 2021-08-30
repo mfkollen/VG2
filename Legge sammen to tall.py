fa178602653f29eaa87d6e0a3c744995cfd3712e
mfkollen <mfkollen@outlook.com>
2021-08-30 08:22:43 +0200

    Lagt til areal av trekant

---
+++ Legge sammen to tall.py
@@ -0,0 +1,9 @@
+Tall1 = 0
+Tall1 = int(input("Gi meg et tall"))
+Tall2 = 0
+Tall2 = int(input("Gi meg et tall til"))
+sum = 0
+sum = int(Tall1) + int(Tall2)
+print("Summen av tall 1 og tall 2 er:" + str (sum))
+
+
