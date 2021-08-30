8dd4e313c2f6b3291f1892b48c164457cdeb80c5
mfkollen <mfkollen@outlook.com>
2021-08-30 08:27:32 +0200

    Multiplisere to tall

---
+++ Multiplisere to tall.py
@@ -0,0 +1,7 @@
+Tall1 = 0
+Tall1 = int(input("Gi meg et tall"))
+Tall2 = 0
+Tall2 = int(input("Gi meg et tall til"))
+sum = 0
+sum = int(Tall1) * int(Tall2)
+print("\n Produktet av tall 1 og tall 2 er:" + str (sum))
